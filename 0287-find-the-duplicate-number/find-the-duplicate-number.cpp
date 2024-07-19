7d860df3fda0750d4ea28f84c03de332ae132261
Himanshu Garg <121446937+himanshu062@users.noreply.github.com>
2024-07-19 14:43:43 +0530

    Time: 105 ms (31.27%) | Memory: 65.1 MB (25.80%)

--- 0287-find-the-duplicate-number/find-the-duplicate-number.cpp
+++ 0287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        for(int i=0;i<nums.size();++i){
-            int ans = nums[i]^nums[i+1];
-            if(ans==0)
-            return nums[i];
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i < nums.size(); ++i) {
+            int ans = nums[i] ^ nums[i + 1];
+            if (ans == 0)
+                return nums[i];
         }
         return -1;
     }
